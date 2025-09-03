a111a4132ea0e726df19be630159f5dd6c249738
anish-eng <anish.kamath2005@gmail.com>
2025-09-02 23:55:50 -0400

    changedhostingmerge

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,14 +6,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions";
 import { getAuth } from "firebase/auth";
-
+// originfal process.env.REACT_APP_API_KEY
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT,
-  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
-  messagingSenderId:process.env.REACT_APP_FIREBASE_MSG,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket:  process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
   
 };
 
